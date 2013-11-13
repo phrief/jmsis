e611706e95222b31a31c8227d41075724dfdc42a
Fritz-Lium <fritzlium@gmail.com>
2013-11-13 11:28:35 +0800

    ddd

--- js/ajax-form.js
+++ js/ajax-form.js
@@ -4,11 +4,11 @@ $.fn.ajaxForm = function(options) {
 		var $form = $(el);
 		$form.on('submit', function(ev) {
 			ev.preventDefault();
-			$.ajax(_.extend({}, {
+			$.ajax(_.extend({
 				url: $form.attr('action'),
 				type: $form.attr('method'),
-				data: $form.serializeJSON()
-			}, options));
+				data: _.extend($form.serializeJSON(), options.data)
+			}, _.omit(options, 'data')));
 		});
 	});
 }
